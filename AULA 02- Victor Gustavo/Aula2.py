6331fe631a1401b4b03ba9f04ab7562b24433cfe
Victor Gustavo <victor.gustavo.vgls@gmail.com>
2024-12-02 10:00:16 -0300

    aprendi o que é uma variável

---
+++ AULA 02- Victor Gustavo/Aula2.py
@@ -0,0 +1,7 @@
+print("Bom dia mundo") #print -> ultilizada pra texto <- ("") 
+
+nome_usuario = 'Victor' #tipo -> str= string
+idade_usuario = 19 #tipo -> int = integer
+altura_usuario = 1.70 #tipo -> float
+maior_de_idade = True #tipo -> bool = boolean True/False
+print("seja bem-vindo", nome_usuario,"sua idade é",idade_usuario,"sua altura", altura_usuario,"você é maior de idade?",maior_de_idade)
